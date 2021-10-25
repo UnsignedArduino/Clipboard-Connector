7f708399bf71fda29dcec1efea7a4283111be578
UnsignedArduino <UnsignedArduino@outlook.com>
2021-10-24 20:46:16 -0400

    Make event loop

--- connector.py
+++ connector.py
@@ -1,7 +1,11 @@
 import logging
+from time import sleep
+
+import networkzero as nw0
 
 from create_logger import create_logger
-import networkzero as nw0
+
+NW0_NEWS_TOPIC = "clipboard_connector"
 
 
 class ClipboardHost:
@@ -20,6 +24,15 @@ class ClipboardHost:
         Kick everything into action!
         """
         self.logger.info(f"Starting host under name \"{self.name}\"")
+        self.logger.debug("Starting event loop")
+        while True:
+            self.service()
+            sleep(1)
+
+    def service(self):
+        """
+        Service everything.
+        """
 
 
 class ClipboardClient:
@@ -32,10 +45,18 @@ class ClipboardClient:
         """
         self.logger = create_logger(name=__name__, level=log_level)
         self.name = name
-        self.logger.debug(f"Target: {self.name}")
 
     def run(self):
         """
         Kick everything into action!
         """
         self.logger.info("Starting client")
+        self.logger.debug("Starting event loop")
+        while True:
+            self.service()
+            sleep(1)
+
+    def service(self):
+        """
+        Service everything.
+        """
