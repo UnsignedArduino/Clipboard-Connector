65a5acd7d2772fd61ba7fea163e36a3fe7bdc80f
UnsignedArduino <UnsignedArduino@outlook.com>
2021-10-24 20:03:37 -0400

    Add verbose option

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import logging
+from argparse import ArgumentParser
+
 from create_logger import create_logger
 
-logger = create_logger(name=__name__, level=logging.DEBUG)
+parser = ArgumentParser(description="Sync text in the clipboard across "
+                                    "multiple computers.")
+parser.add_argument("--verbose", help="Show debug messages",
+                    action="store_true")
+args = parser.parse_args()
+
+level = logging.DEBUG if args.verbose else logging.INFO
+logger = create_logger(name=__name__, level=level)
